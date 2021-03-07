b6222b18292b6db0d10e57b703aac8e3bbf6dee0
Piyush <piyushag@zeta.tech>
2021-03-07 19:26:43 +0530

    adding spring oauth 2.0 project

---
+++ L14_Spring_OAuth2.0/src/main/java/com/example/Controller.java
@@ -0,0 +1,37 @@
+package com.example;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Map<String, Object> answer = new HashMap<>();
+        answer.put("my_name", principal.getAttribute("name"));
+        answer.put("my_company", principal.getAttribute("company"));
+        return answer;
+//		return Collections.singletonMap("name", principal.getAttribute("name"));
+    }
+
+    @RequestMapping("/user_details")
+    public OAuth2User getUserDetails(@AuthenticationPrincipal OAuth2User principal){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return principal;
+    }
+
+    @RequestMapping("/signin")
+    public void signIn(){
+        System.out.println("In the sign in function");
+    }
+}
